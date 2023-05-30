8938393586de016840fa26bccc75bcc7dd2a64ad
Douguinhapi <douglas_p_beccari@estudante.sesisenai.org.br>
2023-05-30 08:24:47 -0300

    nversao

---
+++ var alunos = [].js
@@ -0,0 +1,12 @@
+var alunos = []
+alunos.push ('douglas')
+alunos.push ('jakson')
+alunos.push ('ana')
+alunos.push ('celta')
+alert (alunos)
+alunos [3] = "Edelson" 
+alert (alunos)
+var pos = alunos.indexOf ("ana")
+alunos.splice (pos, 1)
+alert (alunos)
+console.log(alunos)
